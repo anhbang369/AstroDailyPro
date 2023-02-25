f0e5d72cc4d9ff8ace29de932bd7007d9fa4c60b
anhbang369 <107161738+anhbang369@users.noreply.github.com>
2023-02-25 14:39:02 +0700

    add create Aspect,AspectType,AstroProfile,Explain,Horsescope,LifeAtri and getAll Explain and get ID Astroprofile

---
+++ AstroDailyProject/BE-Bang/Controller/HoroscopeController.cs
@@ -0,0 +1,45 @@
+﻿using AstroDailyProject.BE_Bang.Model;
+using AstroDailyProject.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AstroDailyProject.BE_Bang.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HoroscopeController : ControllerBase
+    {
+        private readonly AstroDailyDBContext _context;
+
+        public HoroscopeController(AstroDailyDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateHoroscope(HoroscopeModel horoscopeModel)
+        {
+            try
+            {
+                var horoscope = new Horoscope
+                {
+                    AspectId = horoscopeModel.AspectId,
+                    LifeAttributeId = horoscopeModel.LifeAttributeId,
+                    Date = horoscopeModel.Date,
+                    Description = horoscopeModel.Description
+                };
+                _context.Add(horoscope);
+                _context.SaveChanges(); ;
+                return Ok(horoscope);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
